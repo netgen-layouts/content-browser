f351d2fd6b5214fcbcb2685ff4bca6711857189b
Edi Modrić <edi.modric@gmail.com>
2017-11-02 14:43:14 +0100

    Value provider and backend classes are final

--- lib/Item/ColumnProvider/ColumnValueProvider/Name.php
+++ lib/Item/ColumnProvider/ColumnValueProvider/Name.php
@@ -5,7 +5,7 @@ namespace Netgen\ContentBrowser\Item\ColumnProvider\ColumnValueProvider;
 use Netgen\ContentBrowser\Item\ColumnProvider\ColumnValueProviderInterface;
 use Netgen\ContentBrowser\Item\ItemInterface;
 
-class Name implements ColumnValueProviderInterface
+final class Name implements ColumnValueProviderInterface
 {
     public function getValue(ItemInterface $item)
     {
