3738d7ccacc2f393cfdec24972c90a57d2decef1
Edi Modrić <edi.modric@gmail.com>
2018-01-19 18:52:41 +0100

    Allow running API tests without need for full blown Symfony app

---
+++ tests/bundle/Controller/API/Kernel/AppKernel.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Netgen\Bundle\ContentBrowserBundle\Tests\Controller\API\Kernel;
+
+use Netgen\ContentBrowser\Tests\MockerContainer;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return array(
+            // Symfony
+
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            // Netgen Content Browser
+
+            new \Netgen\Bundle\ContentBrowserBundle\NetgenContentBrowserBundle(),
+        );
+    }
+
+    public function getProjectDir()
+    {
+        return __DIR__;
+    }
+
+    public function getCacheDir()
+    {
+        return sys_get_temp_dir() . '/sfcache';
+    }
+
+    public function getLogDir()
+    {
+        return sys_get_temp_dir() . '/sflogs';
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getProjectDir() . '/config/config.yml');
+    }
+
+    protected function getContainerBaseClass()
+    {
+        return '\\' . MockerContainer::class;
+    }
+}
