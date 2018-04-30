a0420b63486849434a9abf6accf69d550504e0bf
Edi Modrić <edi.modric@gmail.com>
2018-04-30 23:02:52 +0200

    Activate monolog bundle to fix API tests on Symfony 4.1

--- tests/bundle/Controller/API/Kernel/AppKernel.php
+++ tests/bundle/Controller/API/Kernel/AppKernel.php
@@ -16,6 +16,7 @@ final class AppKernel extends Kernel
             new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\MonologBundle\MonologBundle(),
             new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
 
             // Netgen Content Browser
