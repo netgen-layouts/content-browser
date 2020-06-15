a0a7c517969d0437a176042a7220fc0bb8ef312c
Edi Modrić <edi.modric@gmail.com>
2020-06-15 11:51:03 +0200

    Fix Psalm and PHPStan

--- lib/Utils/BackwardsCompatibility/Event.php
+++ lib/Utils/BackwardsCompatibility/Event.php
@@ -5,17 +5,18 @@ declare(strict_types=1);
 namespace Netgen\ContentBrowser\Utils\BackwardsCompatibility;
 
 use Symfony\Component\EventDispatcher\Event as LegacyEvent;
+use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Contracts\EventDispatcher\Event as ContractsEvent;
 use function class_exists;
 
 // Deprecated BC layer for Symfony 4.3 which deprecated Event class from EventDispatcher component.
 // Remove when support for Symfony 3.4 and lower ends.
 
-if (class_exists(ContractsEvent::class)) {
+if (Kernel::VERSION_ID >= 40400) {
     abstract class Event extends ContractsEvent
     {
     }
-} else {
+} elseif (class_exists(LegacyEvent::class)) {
     abstract class Event extends LegacyEvent
     {
     }
