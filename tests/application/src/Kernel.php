ac4d0a9d0c3523ecf9867d213b7d31d42c83be0c
Edi Modric <edi.modric@gmail.com>
2022-09-21 13:09:24 +0200

    Update test app to match Symfony 6

--- tests/application/src/Kernel.php
+++ tests/application/src/Kernel.php
@@ -7,12 +7,9 @@ namespace Netgen\ContentBrowser\Tests\App;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 use function dirname;
-use function is_file;
 use function sys_get_temp_dir;
 
 final class Kernel extends BaseKernel implements CompilerPassInterface
@@ -43,31 +40,6 @@ final class Kernel extends BaseKernel implements CompilerPassInterface
         $container
             ->findDefinition('netgen_content_browser.registry.backend')
             ->setPublic(true);
-    }
-
-    protected function configureContainer(ContainerConfigurator $container): void
-    {
-        $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/' . $this->environment . '/*.yaml');
-
-        if (is_file(dirname(__DIR__) . '/config/services.yaml')) {
-            $container->import('../config/services.yaml');
-            $container->import('../config/{services}_' . $this->environment . '.yaml');
-        } elseif (is_file($path = dirname(__DIR__) . '/config/services.php')) {
-            (require $path)($container->withPath($path), $this);
-        }
-    }
-
-    protected function configureRoutes(RoutingConfigurator $routes): void
-    {
-        $routes->import('../config/{routes}/' . $this->environment . '/*.yaml');
-        $routes->import('../config/{routes}/*.yaml');
-
-        if (is_file(dirname(__DIR__) . '/config/routes.yaml')) {
-            $routes->import('../config/routes.yaml');
-        } elseif (is_file($path = dirname(__DIR__) . '/config/routes.php')) {
-            (require $path)($routes->withPath($path), $this);
-        }
     }
 
     protected function getContainerBaseClass(): string
