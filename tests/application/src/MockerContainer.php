b648a1c25bbff96a97a172de7929884fb0420f77
Edi Modrić <edi.modric@gmail.com>
2025-11-07 12:57:57 +0100

    Use nullsafe operator

--- tests/application/src/MockerContainer.php
+++ tests/application/src/MockerContainer.php
@@ -28,10 +28,8 @@ class MockerContainer extends Container
     public function mock(string $id, object $mock): object
     {
         if (!array_key_exists($id, $this->mockedServices)) {
-            $service = $this->get($id);
-            if ($service === null) {
-                throw new RuntimeException(sprintf('"%s" service does not exist.', $service));
-            }
+            $service = $this->get($id) ??
+                throw new RuntimeException(sprintf('"%s" service does not exist.', $id));
 
             $this->originalServices[$id] = $service;
             $this->mockedServices[$id] = $this->services[$id] = $mock;
